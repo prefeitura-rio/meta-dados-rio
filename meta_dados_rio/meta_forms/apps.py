9b77fd39255796fe34436aa4604e6f893dd0a2c7
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2022-04-25 12:13:50 -0300

    init forms app

---
+++ meta_dados_rio/meta_forms/apps.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from django.apps import AppConfig
+
+
+class MetaFormsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "meta_dados_rio.meta_forms"
