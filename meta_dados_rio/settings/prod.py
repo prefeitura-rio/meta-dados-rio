8fe7332c2fff08386df0ce42230fa1d5df8529bd
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2022-06-02 17:02:01 -0300

    feat: add email debugging

--- meta_dados_rio/settings/prod.py
+++ meta_dados_rio/settings/prod.py
@@ -19,3 +19,13 @@ DATABASES = {
         "PORT": getenv("DB_PORT"),
     }
 }
+
+ADMINS = [("Gabriel Milan", "gabriel.gazola@poli.ufrj.br")]
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = getenv("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = getenv("EMAIL_HOST_USER")
+SERVER_EMAIL = getenv("EMAIL_HOST_USER")
